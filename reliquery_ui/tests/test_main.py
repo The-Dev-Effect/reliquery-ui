af899e2a22de49f7ad768e93762ffa929c7eccae
ultimefrisbee <j.patcooley@gmail.com>
2021-09-19 19:20:17 -0400

    fixed tests

--- reliquery_ui/tests/test_main.py
+++ reliquery_ui/tests/test_main.py
@@ -1,7 +1,8 @@
+from typing import Dict
+
 from fastapi.testclient import TestClient
 
 from ..main import get_app
-from typing import Any, List
 
 
 class TestRelic:
@@ -10,27 +11,43 @@ class TestRelic:
     storage: str
 
     def __init__(
-        self, name: str, relic_type: str, storage: str = None, check_exists=False
+        self,
+        name: str,
+        relic_type: str,
+        storage: str = None,
+        storage_name: str = "default",
+        check_exists: bool = True,
     ):
         self.name = name
         self.relic_type = relic_type
         self.storage = None
+        self.storage_name = storage_name
+        self.check_exists = check_exists
 
     @classmethod
-    def relic_exists(cls, name: str, relic_type: str, storage: str = None) -> bool:
+    def relic_exists(
+        cls,
+        name: str,
+        relic_type: str,
+        storage: str = None,
+        storage_name: str = "default",
+    ) -> bool:
         return True
 
-    def list_arrays(self) -> List[str]:
-        return ["ones"]
+    def describe(self) -> Dict:
+        return {"tutorial": {"arrays": [], "text": [], "html": []}}
 
 
 def test_read_relic():
     app = get_app(TestRelic)
     client = TestClient(app)
-    response = client.get("/api/reliquery/basic/tutorial")
+    response = client.get("/api/reliquery/default/basic/tutorial")
     assert response.status_code == 200
+    print(response)
     assert response.json() == {
         "name": "tutorial",
         "relic_type": "basic",
-        "arrays": ["ones"],
+        "arrays": [],
+        "text": [],
+        "html": [],
     }
