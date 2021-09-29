bdf98584c5f74dec774dce3d6373615bb8c6f4c3
welsh <welsh@pop-os.localdomain>
2021-09-29 17:06:11 -0400

    c

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name="reliquery_ui",
-    version="0.1.6b",
+    version="0.1.6c",
     description="Science's Artifact Antiformat",
     url="https://github.com/The-Dev-Effect/reliquery-ui",
     author="The Dev Effect",
