e018fac6d88bd84456436d648b09174666f4517d
welsh <welsh@pop-os.localdomain>
2021-09-29 16:58:14 -0400

    fixed docker syntax again

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name="reliquery_ui",
-    version="0.1.6a",
+    version="0.1.6b",
     description="Science's Artifact Antiformat",
     url="https://github.com/The-Dev-Effect/reliquery-ui",
     author="The Dev Effect",
