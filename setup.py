364112dfc7e5b001a9ea762c7d05af7ac6337413
welsh <welsh@pop-os.localdomain>
2021-09-30 10:25:12 -0400

    update version to a1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name="reliquery_ui",
-    version="0.1.6d",
+    version="0.1.6a1",
     description="Science's Artifact Antiformat",
     url="https://github.com/The-Dev-Effect/reliquery-ui",
     author="The Dev Effect",
