13b71caf341f1c1250b303a06fded71260dd9592
welsh <welsh@pop-os.localdomain>
2021-11-23 09:49:59 -0500

    fixed base url to work in a docker image

--- frontend/path_config.ts
+++ frontend/path_config.ts
@@ -1,3 +1,6 @@
 // change path based on env 
 // DefinePlugin to set variables in the webpack.config.js
-export const base_path = " http://localhost:8000"
+declare const DEVELOPMENT: boolean;
+declare const BASE_PATH: string;
+
+export const base_path = DEVELOPMENT ? BASE_PATH : "";
